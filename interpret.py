3ef36b15b46594be14770ad35260d45828b989eb
Marián Tarageľ <mariantaragel@gmail.com>
2023-02-26 16:17:52 +0100

    Code rewrited to OOP

--- interpret.py
+++ interpret.py
@@ -1,4 +1,31 @@
 ## @file interpret.py
-# @brief IPPcode21 interpreter
+# @brief IPPcode23 interpreter
 # @author Marián Tarageľ
-# @date 18.2.2023
+# @date 23.2.2023
+
+import argparse
+import sys
+class Myargparse(argparse.ArgumentParser):
+    def error(self, message):
+        self.print_usage(sys.stderr)
+        args = {'prog': self.prog, 'message': message}
+        self.exit(10, ('%(prog)s: error: %(message)s\n') % args)
+
+def handle_error(error_code):
+    print('Error: ' + str(error_code))
+    exit(error_code)
+
+parser = Myargparse(formatter_class=argparse.RawDescriptionHelpFormatter, description="""
+Skript načíta XML reprezentáciu programu a tento program s využitím vstupu
+podľa parametrov príkazového riadku interpretuje a generuje výstup.""")
+parser.add_argument('--source', metavar='FILE', dest='source', default='STDIN',
+help="vstupný súbor s XML reprezentaciou zdrojového kódu")
+parser.add_argument('--input', metavar='FILE', dest='input', default='STDIN',
+help="soubor se vstupmi pre samotnú interpretáciu zadaného zdrojového kódu")
+
+args = parser.parse_args()
+if args.source == 'STDIN' and args.input == 'STDIN':
+    parser.error('no arguments')
+
+print(args.source)
+print(args.input)
--- parse.php
+++ parse.php
@@ -3,297 +3,359 @@
  * @file parse.php
  * @brief IPPcode23 analyser
  * @author Marián Tarageľ
- * @date 18.2.2023
+ * @date 26.2.2023
  */
 
 ini_set('display_errors', 'stderr');
 
-parse_args($argc, $argv);
-parse_header();
+class Parser extends DataType
+{
+    private $header = ".IPPCODE23";
+    private $help_message = "usage: parse.php [--help]
+A filter script reads the source code in IPPcode23 from standard input, checks
+the lexical and syntactic correctness of the code and writes to standard output
+XML representation of the program.
 
-$xmlheader = '<?xml version="1.0" encoding="utf-8"?><program></program>';
-$program = new SimpleXMLElement($xmlheader);
-$program->addAttribute('language', 'IPPcode23');
+-h, --help\tshow this help message and exit\n";
 
+    // Argument parser
+    public function parse_args($argc, $argv)
+    {
+        if ($argc > 2) {
+            $this->error_handler(10);
+        }
+        
+        if ($argc == 2) {
+            if ($argv[1] == "-h" || $argv[1] == "--help") {
+                $this->help();
+            }
+            else {
+                $this->error_handler(10);
+            }
+        }
+    }
+
+    // Parse header .IPPcode23
+    public function parse_header()
+    {
+        while ($header = fgets(STDIN)) {
+            $header = preg_replace('/\s*/', "", $header);
+            $header = preg_replace('/#.*/', "", $header);
+            if (strlen($header) > 0) {
+                if (strtoupper($header) == $this->header) {
+                    return;
+                }
+                else {
+                    $this->error_handler(21);
+                }
+            }
+        }
+        $this->error_handler(21);
+    }
+
+    // Handling errors
+    public function error_handler($error_code)
+    {
+        fprintf(STDERR, "Error: " . $error_code . "\n");
+        exit($error_code);
+    }
+
+    // Print help message
+    private function help()
+    {
+        echo $this->help_message;
+        exit(0);
+    }
+
+    // One line of source code will be transformed to token array
+    public function scan($line)
+    {
+        $instruction = preg_replace('/#.*/', "", $line);
+        $instruction = trim($instruction);
+        if (strlen($instruction) == 0) {
+            return array();
+        }
+        $instruction = preg_replace('/\s+/', " ", $instruction);
+        $instruction = explode(" ", $instruction);
+        $instruction[0] = strtoupper($instruction[0]);
+        return $instruction;
+    }
+
+    // Check syntax of instructions with no arguments
+    public function check_opcode($instruction)
+    {
+        if (count($instruction) != 1) {
+            $this->error_handler(23);
+        }
+    }
+
+    // Check syntax of instruction with label argument
+    public function check_opcode_label($instruction)
+    {
+        if (count($instruction) != 2) {
+            $this->error_handler(23);
+        }
+        if (!$this->is_label($instruction[1])) {
+            $this->error_handler(23);
+        }
+    }
+
+    // Check syntax of instruction with symb argument
+    public function check_opcode_symb($instruction)
+    {
+        if (count($instruction) != 2) {
+            $this->error_handler(23);
+        }
+        if (!$this->is_symb($instruction[1])) {
+            $this->error_handler(23);
+        }
+    }
+
+    // Check syntax of instruction with var argument
+    public function check_opcode_var($instruction)
+    {
+        if (count($instruction) != 2) {
+            $this->error_handler(23);
+        }
+        if (!$this->is_var($instruction[1])) {
+            $this->error_handler(23);
+        }
+    }
+
+    // Check syntax of instruction with two arguments var and symb
+    public function check_opcode_var_symb($instruction)
+    {
+        if (count($instruction) != 3) {
+            $this->error_handler(23);
+        }
+        if (!$this->is_var($instruction[1]) || !$this->is_symb($instruction[2])) {
+            $this->error_handler(23);
+        }
+    }
+
+    // Check syntax of instruction with two arguments var and type
+    public function check_opcode_var_type($instruction)
+    {
+        if (count($instruction) != 3) {
+            $this->error_handler(23);
+        }
+        if (!$this->is_var($instruction[1]) || !$this->is_type($instruction[2])) {
+            $this->error_handler(23);
+        }
+    }
+
+    // Check syntax of instruction with three arguments label, symb and symb
+    public function check_opcode_label_symb_symb($instruction)
+    {
+        if (count($instruction) != 4) {
+            $this->error_handler(23);
+        }
+        if (!$this->is_label($instruction[1]) ||
+            !$this->is_symb($instruction[2]) ||
+            !$this->is_symb($instruction[3])) {
+            $this->error_handler(23);
+        }
+    }
+
+    // Check syntax of instruction with three arguments var, symb and symb
+    public function check_opcode_var_symb_symb($instruction)
+    {
+        if (count($instruction) != 4) {
+            $this->error_handler(23);
+        }
+        if (!$this->is_var($instruction[1]) ||
+            !$this->is_symb($instruction[2]) ||
+            !$this->is_symb($instruction[3])) {
+            $this->error_handler(23);
+        }
+    }
+}
+
+class DataType
+{
+    private $type_regex = '/^(int|string|bool)$/';
+    private $var_regex = '/^(GF|LF|TF)@([a-zA-Z]|(_|-|\$|&|%|\*|!|\?))([a-zA-Z0-9]|(_|-|\$|&|%|\*|!|\?))*$/';
+    private $label_regex = '/^([a-zA-Z]|(_|-|\$|&|%|\*|!|\?))([a-zA-Z0-9]|(_|-|\$|&|%|\*|!|\?))*$/';
+    private $nil_regex = '/^nil@nil$/';
+    private $lit_bool_regex = '/^bool@(true|false)$/';
+    private $lit_string_regex = '/^string@([^#\s\\\]*(\\\[0-9][0-9][0-9])*)*$/';
+    private $lit_int_decimal_regex = '/^int@(-|\+)?\d+$/';
+    private $lit_int_hexadecimal_regex = '/^int@0[xX][\da-fA-F]+$/';
+    private $lit_int_octal_regex = '/^int@0[oO][0-7]+$/';
+
+    // Match type
+    public function is_type($type) {
+        return preg_match($this->type_regex, $type);
+    }
+
+    // Match var
+    public function is_var($var) {
+        return preg_match($this->var_regex, $var);
+    }
+
+    // Match label
+    public function is_label($label) {
+        return preg_match($this->label_regex, $label);
+    }
+
+    // Match symb
+    public function is_symb($symb)
+    {
+        if ($this->is_var($symb) || $this->is_nil($symb)) {
+            return 1;
+        }
+        elseif ($this->is_lit_bool($symb) || $this->is_lit_int($symb) || $this->is_lit_string($symb)) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    // Match nil
+    public function is_nil($nil) {
+        return preg_match($this->nil_regex, $nil);
+    }
+
+    // Match lit bool
+    public function is_lit_bool($bool) {
+        return preg_match($this->lit_bool_regex, $bool);
+    }
+
+    // Match lit string
+    public function is_lit_string($string) {
+        return preg_match($this->lit_string_regex, $string);
+    }
+
+    // Match lit int (decimal, hexadecimal and octal)
+    public function is_lit_int($int)
+    {
+        if (preg_match($this->lit_int_decimal_regex, $int)) {
+            return 1;
+        }
+        elseif (preg_match($this->lit_int_hexadecimal_regex, $int)) {
+            return 1;
+        }
+        else {
+            return preg_match($this->lit_int_octal_regex, $int);
+        }
+    }
+
+    // Return type of token
+    public function get_type($token)
+    {
+        if ($this->is_var($token)) {
+            return "var";
+        }
+        elseif ($this->is_type($token)) {
+            return "type";
+        }
+        elseif ($this->is_label($token)) {
+            return "label";
+        }
+        else {
+            $token_divided = explode("@", $token);
+            return $token_divided[0];
+        }
+    }
+}
+
+class XMLGenerator
+{
+    protected $simpleXML;
+    protected $data;
+
+    // XMLGenerator constructor
+    public function __construct($simpleXML, $data)
+    {
+        $this->simpleXML = $simpleXML;
+        $this->data = $data;
+    }
+
+    // Generate XML of valid instruction
+    public function generate_xml($token_array)
+    {
+        static $order = 1;
+        $instruction = $this->simpleXML->addChild('instruction');
+        $instruction->addAttribute('order', $order);
+        $instruction->addAttribute('opcode', $token_array[0]);
+        $order++;
+        $this->add_args($instruction, $token_array);
+    }
+
+    // Print XML to stdout
+    public function print_xml()
+    {
+        $dom = dom_import_simplexml($this->simpleXML)->ownerDocument;
+        $dom->formatOutput = true;
+        echo $dom->saveXML($dom, LIBXML_NOEMPTYTAG);
+    }
+
+    // Add attribute language with value
+    public function add_lang_code($lang, $value) {
+        $this->simpleXML->addAttribute($lang, $value);
+    }
+
+    // Add one agrument of instruction to XML
+    private function add_arg($instruction, $type, $num, $value)
+    {
+        $arg = $instruction->addChild('arg' . $num, $value);
+        $arg->addAttribute('type', $type);
+    }
+
+    // Add more arguments of instruction to XML
+    private function add_args($instruction, $args)
+    {
+        for ($i = 1; $i < count($args); $i++) {
+            $type = $this->data->get_type($args[$i]);
+            $args[$i] = str_replace('&', '&amp;', $args[$i]);
+            $value = explode("@", $args[$i], 2);
+            if ($this->data->is_type($type) || $type == "nil") {
+                $this->add_arg($instruction, $type, $i, $value[1]);
+            }
+            else {
+                $this->add_arg($instruction, $type, $i, $args[$i]);
+            }
+        }
+    }
+}
+
+// Creating objects
+$xml_header = '<?xml version="1.0" encoding="utf-8"?><program></program>';
+$simpleXML = new SimpleXMLElement($xml_header);
+$data = new DataType();
+$program = new XMLGenerator($simpleXML, $data);
+$program->add_lang_code('language', 'IPPcode23');
+$parser = new Parser();
+
+$parser->parse_args($argc, $argv);
+$parser->parse_header();
+
+// Read source code by lines
 while ($line = fgets(STDIN)) {
-    $token_array = scan($line);
+    $token_array = $parser->scan($line);
     if (count($token_array) < 1) {
         continue;
     }
-    switch ($token_array[0]) {
-        case "MOVE":
-        case "NOT":
-        case "INT2CHAR":
-        case "STRLEN":
-        case "TYPE":
-            if (count($token_array) != 3) {
-                error(23);
-            }
-            if (!is_var($token_array[1]) || !is_symb($token_array[2])) {
-                error(23);
-            }
-            break;
-        
-        case "CREATEFRAME":
-        case "PUSHFRAME":
-        case "POPFRAME":
-        case "RETURN":
-        case "BREAK":
-            if (count($token_array) != 1) {
-                error(23);
-            }
-            break;
-    
-        case "DEFVAR":
-        case "POPS":
-            if (count($token_array) != 2) {
-                error(23);
-            }
-            if (!is_var($token_array[1])) {
-                error(23);
-            }
-            break;
 
-        case "CALL":
-        case "LABEL":
-        case "JUMP":
-            if (count($token_array) != 2) {
-                error(23);
-            }
-            if (!is_label($token_array[1])) {
-                error(23);
-            }
-            break;
-        
-        case "PUSHS":
-        case "WRITE":
-        case "EXIT":
-        case "DPRINT":
-            if (count($token_array) != 2) {
-                error(23);
-            }
-            if (!is_symb($token_array[1])) {
-                error(23);
-            }
-            break;
-        
-        case "ADD":
-        case "SUB":
-        case "MUL":
-        case "IDIV":
-        case "LT":
-        case "GT":
-        case "EQ":
-        case "AND":
-        case "OR":
-        case "STRI2INT":
-        case "CONCAT":
-        case "GETCHAR":
-        case "SETCHAR":
-            if (count($token_array) != 4) {
-                error(23);
-            }
-            if (!is_var($token_array[1]) || !is_symb($token_array[2]) || !is_symb($token_array[3])) {
-                error(23);
-            }
-            break;
-        
-        case "READ":
-            if (count($token_array) != 3) {
-                error(23);
-            }
-            if (!is_var($token_array[1]) || !is_type($token_array[2])) {
-                error(23);
-            }
-            break;
-        
-        case "JUMPIFEQ":
-        case "JUMPIFNEQ":
-            if (count($token_array) != 4) {
-                error(23);
-            }
-            if (!is_label($token_array[1]) || !is_symb($token_array[2]) || !is_symb($token_array[3])) {
-                error(23);
-            }
-            break;
-        
-        default:
-            error(22);
-            break;
-    }
-    generate_xml($program, $token_array);
+    // Based on first token check syntax of instructions
+    match($token_array[0]) {
+        "MOVE", "NOT", "INT2CHAR", "STRLEN", "TYPE" => $parser->check_opcode_var_symb($token_array),
+        "CREATEFRAME", "PUSHFRAME", "POPFRAME", "RETURN", "BREAK" => $parser->check_opcode($token_array),
+        "DEFVAR", "POPS" => $parser->check_opcode_var($token_array),
+        "CALL", "LABEL", "JUMP" => $parser->check_opcode_label($token_array),
+        "PUSHS", "WRITE", "EXIT", "DPRINT" => $parser->check_opcode_symb($token_array),
+        "ADD", "SUB", "MUL", "IDIV", "LT", "GT", "EQ", "AND", "OR", "STRI2INT", "CONCAT", "GETCHAR", "SETCHAR" =>
+            $parser->check_opcode_var_symb_symb($token_array),
+        "READ" => $parser->check_opcode_var_type($token_array),
+        "JUMPIFEQ", "JUMPIFNEQ" => $parser->check_opcode_label_symb_symb($token_array),
+        default => $parser->error_handler(22)
+    };
+    
+    $program->generate_xml($token_array);
 }
 
-$dom = dom_import_simplexml($program)->ownerDocument;
-$dom->formatOutput = true;
-echo $dom->saveXML($dom, LIBXML_NOEMPTYTAG);
-
+$program->print_xml();
 exit(0);
 
-function add_arg($instruction, $type, $num, $value)
-{
-    $arg = $instruction->addChild('arg' . $num, $value);
-    $arg->addAttribute('type', $type);
-}
-
-function get_type($token)
-{
-    if (is_var($token)) {
-        return "var";
-    }
-    elseif (is_type($token)) {
-        return "type";
-    }
-    elseif (is_label($token)) {
-        return "label";
-    }
-    else {
-        $token_divided = explode("@", $token);
-        return $token_divided[0];
-    }
-}
-
-function add_args($instruction, $args)
-{
-    for ($i = 1; $i < count($args); $i++) {
-        $type = get_type($args[$i]);
-        $args[$i] = str_replace('&', '&amp;', $args[$i]);
-        $value = explode("@", $args[$i], 2);
-        if (is_type($type) || $type == "nil") {
-            add_arg($instruction, $type, $i, $value[1]);
-        }
-        else {
-            add_arg($instruction, $type, $i, $args[$i]);
-        }
-    }
-}
-
-function generate_xml($program, $token_array)
-{
-    static $order = 1;
-    $instruction = $program->addChild('instruction');
-    $instruction->addAttribute('order', $order);
-    $instruction->addAttribute('opcode', $token_array[0]);
-    $order++;
-    add_args($instruction, $token_array);
-}
-
-function is_type($type)
-{
-    return preg_match('/^(int|string|bool)$/', $type);
-}
-
-function is_var($var)
-{
-    return preg_match('/^(GF|LF|TF)@([a-zA-Z]|(_|-|\$|&|%|\*|!|\?))([a-zA-Z0-9]|(_|-|\$|&|%|\*|!|\?))*$/', $var);
-}
-
-function is_nil($nil)
-{
-    return preg_match('/^nil@nil$/', $nil);
-}
-
-function is_lit_bool($bool)
-{
-    return preg_match('/^bool@(true|false)$/', $bool);
-}
-
-function is_lit_int($int)
-{
-    if (preg_match('/^int@(-|\+)?\d+$/', $int)) {
-        return 1;
-    }
-    elseif (preg_match('/^int@0[xX][\da-fA-F]+$/', $int)) {
-        return 1;
-    }
-    else {
-        return preg_match('/^int@0[oO][0-7]+$/', $int);
-    }
-}
-
-function is_lit_string($string)
-{
-    return preg_match('/^string@([^#\s\\\]*(\\\[0-9][0-9][0-9])*)*$/', $string);
-}
-
-function is_symb($symb)
-{
-    if (is_var($symb) || is_nil($symb)) {
-        return 1;
-    }
-    elseif (is_lit_bool($symb) || is_lit_int($symb) || is_lit_string($symb)) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
-}
-
-function is_label($label)
-{
-    return preg_match('/^([a-zA-Z]|(_|-|\$|&|%|\*|!|\?))([a-zA-Z0-9]|(_|-|\$|&|%|\*|!|\?))*$/', $label);
-}
-
-function error($error_code)
-{
-    echo "Error: " . $error_code . "\n";
-    exit($error_code); 
-}
-
-function scan($line)
-{
-    $instruction = preg_replace('/#.*/', "", $line);
-    $instruction = trim($instruction);
-    if (strlen($instruction) == 0) {
-        return array();
-    }
-    $instruction = preg_replace('/\s+/', " ", $instruction);
-    $instruction = explode(" ", $instruction);
-    $instruction[0] = strtoupper($instruction[0]);
-    return $instruction;
-}
-
-function parse_header()
-{
-    while ($header = fgets(STDIN)) {
-        $header = preg_replace('/\s*/', "", $header);
-        $header = preg_replace('/#.*/', "", $header);
-        if (strlen($header) > 0) {
-            if (strtoupper($header) == ".IPPCODE23") {
-                return;
-            }
-            else {
-                error(21);
-            }
-        }
-    }
-    error(21);
-}
-
-function help()
-{
-    printf("Použitie: php8.1 parse.php [--help]
-Skript typu filter načíta zo štandardného vstupu zdrojový kód v IPPcode23,
-skontroluje lexikálnu a syntaktickú správnosť kódu a vypíše na štandardný
-výstup XML reprezentáciu programu.
-        
--help, --help\tzobrazí pomocníka a skončí\n");
-    exit(0);
-}
-
-function parse_args($argc, $argv)
-{
-    if ($argc > 2) {
-        error(10);
-    }
-    
-    if ($argc == 2) {
-        if ($argv[1] == "-help" || $argv[1] == "--help") {
-            help();        
-        }
-        else {
-            error(10);
-        }
-    }
-}
 ?>
